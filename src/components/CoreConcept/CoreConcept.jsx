9691982cafe4fd519b0ef72dcd1aea1987cd4949
Zach <zachare18@gmail.com>
2025-05-23 00:10:52 -0400

    Adding missing work from last commit

---
+++ src/components/CoreConcept/CoreConcept.jsx
@@ -0,0 +1,10 @@
+
+export function CoreConcept({ image, description, title }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
